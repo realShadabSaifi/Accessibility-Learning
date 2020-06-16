01836c65247679bc02d4279ba009205a8ca591fb
Ajay Sohal <ajay@pivotalaccessibility.com>
2020-06-16 17:07:22 +0530

    Tab implementation.

--- main.js
+++ main.js
@@ -14,4 +14,60 @@ window.onload = function () {
         }
     });
 
+    function handleChanges() {
+
+        $("#a11y-tablist button").each(function () {
+
+            const tabpanel = $(this).attr("data-id");
+
+            if ($(this).hasClass("active")) {
+                $("#" + tabpanel).show();
+
+                // Keyboard support
+                $(this).attr({ "tabindex": 0, "aria-selected": "true", "aria-controls": tabpanel });
+
+            } else {
+                $("#" + tabpanel).hide();
+
+                // Keyboard support
+                $(this).attr({ "tabindex": -1, "aria-selected": "false" });
+                $(this).removeAttr("aria-controls");
+
+            }
+        });
+    }
+
+    handleChanges();
+
+    $("#a11y-tablist button").click(function () {
+
+        $("#a11y-tablist > button.active").removeClass('active');
+        $(this).addClass('active');
+
+        handleChanges();
+    });
+
+
+    $("#a11y-tablist button").keydown(function (event) {
+        const keyCode = event.which;
+
+        switch (keyCode) {
+            case 39:
+                $(this).next().focus();
+                $(this).next().click();
+                event.preventDefault();
+                break;
+
+            case 37:
+                $(this).prev().focus();
+                $(this).prev().click();
+                event.preventDefault();
+                break;
+
+            default:
+                break;
+        }
+
+    });
+
 }
